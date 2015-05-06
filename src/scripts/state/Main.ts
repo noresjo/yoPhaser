76046e5b99050399971fe7419430553806e8b046
noresjo <noresjo@gmail.com>
2015-05-06 23:34:58 +0200

    Something should happen at least

--- src/scripts/state/Main.ts
+++ src/scripts/state/Main.ts
@@ -2,10 +2,16 @@
 
 module YoPhaser.State {
   export class Main extends Phaser.State {
-
+    text: Phaser.Text;
+    
     create() {
       var thing:String = 'code !';
-      this.add.text(10, 10, `Let's ${thing}`, { font: '65px Arial' });
+      this.text = this.add.text(10, 10, `Let's ${thing}`, { font: '65px Arial' });
+    }
+    
+    update() {
+      var sizeOfMovement = this.game.width - this.text.width;
+      this.text.x = sizeOfMovement / 2 + Math.sin(this.game.time.now / 1000) * sizeOfMovement /2;
     }
   }
 }
